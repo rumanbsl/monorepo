03f5963be9ab66badb92c9d241d880010483ef7c
Majedul Hoque <rumanbsl@gmail.com>
2019-10-22 23:00:32 +0300

    ok

--- mobile/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function babelConfig(api) {
-  api.cache(true);
-  return {
-    extends : "../babel.config.js",
-    presets : [["@babel/env", { targets: { esmodules: true } }]],
-  };
-};
