bb122f0c0fad964889e074172db35eeeea5522a6
Majedul Hoque <rumanbsl@gmail.com>
2019-11-16 16:33:43 +0200

    started with env variables

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -6,7 +6,11 @@ use mongodb::{
     Client, ThreadedClient,
 };
 use routes::setup_routes;
-use std::sync::Mutex;
+use std::{
+    env,
+    io::{Error, ErrorKind},
+    sync::Mutex,
+};
 use tide::{middleware::RootLogger, App};
 
 pub struct State {
@@ -19,16 +23,42 @@ impl State {
     }
 }
 
-fn main() {
-    println!("wtf");
-    // database => docker localhost
-    let mongo =
-        Client::connect("database", 27017).expect("Failed to initialize standalone client.");
+fn main() -> Result<(), Error> {
+    let config = Config::get()?;
+
+    let mongo = Client::connect(&config.db_host[..], 27017)
+        .expect("Failed to initialize standalone client.");
     mongo.db("tide").collection("hello");
-    let mut app = App::with_state(State::new(Mutex::new(mongo.db("tide"))));
+    let mut app = App::with_state(State::new(Mutex::new(mongo.db("docker-ts"))));
     app.middleware(RootLogger::new());
 
     setup_routes(&mut app);
+    Ok(app.serve(format!("0.0.0.0:{}", config.server_port))?)
+}
 
-    app.serve("0.0.0.0:1212").unwrap();
+#[derive(Default)]
+struct Config {
+    db_host: String,
+    server_port: String,
+}
+
+impl Config {
+    fn get() -> Result<Config, Error> {
+        for (key, value) in env::vars() {
+            println!("{}: {}", key, value);
+        }
+        let mut config = Config::default();
+        if let Ok(port) = env::var("SERVER_PORT") {
+            config.server_port = port;
+        } else {
+            return Err(Error::new(ErrorKind::NotFound, "No server port found"));
+        };
+        if let Ok(db_host) = env::var("HOST_DB") {
+            config.db_host = db_host;
+        } else {
+            return Err(Error::new(ErrorKind::NotFound, "No db host found"));
+        };
+
+        Ok(config)
+    }
 }
